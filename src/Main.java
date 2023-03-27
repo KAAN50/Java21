ed34d2e1df841f80dc8158ca9c6e212be7764ac0
KAAN50 <skaan45145@gmail.com>
2023-03-27 16:47:19 +0300

    Java21

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz :");
+        int n = klavye.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+        for (int a = 1; a <= n - 1; a++) {
+            for (int b = 1; b <= a; b++) {
+                System.out.print(" ");
+            }
+            for (int c = 1; c <= 2 * (n - a) - 1; c++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+
+
+        }
+    }
+}
+      
+
